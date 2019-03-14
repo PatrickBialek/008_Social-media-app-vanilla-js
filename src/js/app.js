c2d3332d0e631b1635c76368c83daa1944eaab48
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-03-14 16:16:47 +0100

    Working on core.js structure

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,19 @@ import {
 	HTML
 } from "./modules/html";
 
+import {
+	CORE
+} from "./modules/core";
+
 const html = new HTML(),
+	core = new CORE(),
 	switchToSignUp = document.querySelector('#switch-to-sign-up'),
 	switchToSignIn = document.querySelector('#switch-to-sign-in');
 
 console.log(html);
+console.log(core);
 
 export {
-	html
+	html,
+	core
 };
---
+++ src/js/modules/core.js
@@ -0,0 +1,46 @@
+// This import loads the firebase namespace.
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+import {
+	core
+} from '../app';
+
+class CORE {
+	initializeFirebase() {
+
+	}
+
+	checkIfUserIsLogIn() {
+
+	}
+
+	signIn() {
+
+	}
+
+	signUp() {
+
+	}
+
+	resetUserPassword() {
+
+	}
+
+	continueWithFacebook() {
+
+	}
+
+	continueWithGoogle() {
+
+	}
+
+	signOut() {
+
+	}
+}
+
+export {
+	CORE
+};
