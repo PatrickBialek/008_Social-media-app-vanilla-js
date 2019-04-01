51c6b575ace8d93e2c46f918e23b00625de50cb9
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-01 19:19:08 +0200

    working on rwd intro and settings part 2

--- src/js/independent-scripts/animations.js
+++ src/js/independent-scripts/animations.js
@@ -16,4 +16,27 @@ setTimeout(() => {
 		userAvatar.addEventListener('click', menuHandler);
 	}
 
-}, 1500);
+	const introSildeDownArrow = document.querySelector('#profile-intro-slide-control');
+	const settingsSlideDownArrow = document.querySelector('#settings-slide-control');
+
+	function introHandler() {
+		if (document.body.classList.contains('intro-slide-down')) {
+			document.body.classList.remove('intro-slide-down');
+		} else {
+			document.body.classList.add('intro-slide-down');
+		}
+	}
+
+	function settingsHandler() {
+		if (document.body.classList.contains('settings-slide-down')) {
+			document.body.classList.remove('settings-slide-down');
+		} else {
+			document.body.classList.add('settings-slide-down');
+		}
+	}
+
+
+	introSildeDownArrow.addEventListener('click', introHandler);
+	settingsSlideDownArrow.addEventListener('click', settingsHandler);
+
+}, 2500);
